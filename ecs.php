d045f179df58e4b03fec7c0443e162d0fc6ae7e7
Joachim Løvgaard <joachim@loevgaard.dk>
2023-09-18 10:50:40 +0200

    Upgrade Sylius application 1.10

---
+++ ecs.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+
+return static function (ContainerConfigurator $config): void {
+    $config->import('vendor/sylius-labs/coding-standard/ecs.php');
+    $config->parameters()->set(Option::PATHS, [
+        'src',
+        'tests'
+    ]);
+    $config->parameters()->set(Option::SKIP, [
+        'tests/Application/**'
+    ]);
+};
